fe19593b49b161a420be86373fe18ddc56fd9885
Bob <bob.e.baxter@gmail.com>
2019-05-19 20:49:25 -0500

    dropdown works

---
+++ src/javascripts/components/navbar/navbar.js
@@ -0,0 +1,41 @@
+import $ from 'jquery';
+import util from '../../helpers/util';
+import categoriesData from '../../helpers/data/categoriesData';
+import typesModule from '../types/types';
+
+const seeTypeDiv = (e) => {
+  e.preventDefault();
+  const categoryName = e.target.innerText;
+  const categoryId = e.target.id;
+  $('#categories-page').addClass('hide');
+  $('#types-page').removeClass('hide');
+  typesModule.initTypes(categoryName, categoryId);
+};
+
+const bindCategoriesEvents = () => {
+  const allButtons = $('.nav-see-types');
+  for (let i = 0; i < allButtons.length; i += 1) {
+    allButtons[i].addEventListener('click', seeTypeDiv);
+  }
+};
+
+const printNavbarCategories = (categories) => {
+  let domString = '';
+  $.each(categories, (i) => {
+    domString += `<a id="${categories[i].id}" class="dropdown-item nav-see-types" href="#">${categories[i].name}</a>`;
+  });
+  util.printToDom('navbar-categories', domString);
+  bindCategoriesEvents();
+};
+
+const initCategories = () => {
+  categoriesData.loadCategories()
+    .then(resp => printNavbarCategories(resp.data.categories))
+    .catch(err => console.error('error from navBar initCategories', err));
+};
+
+const init = () => {
+  initCategories();
+};
+
+export default { init };
--- src/javascripts/helpers/data/productsData.js
+++ src/javascripts/helpers/data/productsData.js
@@ -1,9 +1,63 @@
 import axios from 'axios';
+// import typesData from './typesData';
+// import categoriesData from './categoriesData';
+
+/*
+This is an async failure I'm keeping in the notes, so I can eventually come back and see what I was
+doing wrong.
+
+
+I was trying to grab the .json information from the categories and types and add them to the
+product type at the point when the products were loaded after clicking one of the type buttons;
+however, the async call seems to be returning after I print to the DOM and I can't seem to figure
+out an Ecmascript 6 way to async/await for the promise to resolve before my code moves on and prints
+to the DOM.
+
+The goal was to only print the Categories in Navbar dropdown, which doesn't affect the products
+information, which I pass through the functions to the ending product cards.  However, as a stretch
+goal, I was trying to also print the Types in the Navbar, but I've spent all day and just need to
+finish this project.
+*/
+
+// const syncProductsInfo = (products) => {
+//   const syncedProductList = [];
+//   const buildProducts = (types, categories) => {
+//     products.map((product) => {
+//       const newProduct = product[1];
+//       const matchingType = types.filter(type => newProduct.typeId === type.id);
+//       newProduct.categoryId = matchingType[0].categoryId;
+//       newProduct.typeName = matchingType[0].name;
+//       const matchingCategory = categories.filter(cat => newProduct.categoryId === cat.id);
+//       newProduct.categoryName = matchingCategory[0].name;
+//       return syncedProductList.push(newProduct);
+//     });
+//   };
+//   const getTypes = () => new Promise((resolve, reject) => {
+//     axios.get('../db/types.json')
+//       .then((resp) => {
+//         const { types } = resp.data;
+//         axios.get('../db/categories.json')
+//           .then((response) => {
+//             const { categories } = response.data;
+//             resolve([types, categories]);
+//           })
+//           .catch(err => console.error(err));
+//       })
+//       .catch(err => reject(err));
+//   });
+//   getTypes()
+//     .then((typesAndCats) => {
+//       buildProducts(typesAndCats[0], typesAndCats[1]);
+//     })
+//     .catch(err => console.error(err));
+//   return syncedProductList;
+// };
 
 const loadProductsFromType = typeId => new Promise((resolve, reject) => {
   axios.get('../db/products.json')
     .then((resp) => {
       const allProducts = Object.entries(resp.data.products[0]);
+      // syncProductsInfo(allProducts);
       const matchingProducts = allProducts.filter(product => product[1].typeId === typeId);
       resolve(matchingProducts);
     })
